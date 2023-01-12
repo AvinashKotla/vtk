d13a48e08afb91a6eb80554b40b32818e9297b8a
AvinashKotla <94164266+AvinashKotla@users.noreply.github.com>
2023-01-13 00:14:54 +0530

    Add files via upload

---
+++ online/IO/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+
+string q = "This is a string";
+
+cout << q << endl;
+
+cout << q.c_str() << endl;
+
+return 0;
+
+}
